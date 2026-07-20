7e6b8a7441b6239322bdcedb566d1e8272389e69
yangminglong <ilyyangminglong@126.com>
2026-07-20 18:52:39 +0800

    更新注释

--- include/pd_config.h
+++ include/pd_config.h
@@ -3,10 +3,10 @@
 #include <stdint.h>
 #include "pd_types.h"
 
-#define GPIO_PD 4  //  状态指示？
-// #define GPIO_CC1 3 // 通过与GPIO_TX连接来得到一个大约1.7V的分压器, ESP32内部上拉、下拉电阻为47K
-#define GPIO_TX 3 // ^-^ 连接USB-CC. we now have configured the GPIO_CC1 pin and GPIO_TX pin to drive against each other. with these drive capabilities, we get a voltage divider to approx 1.7V, which is closer to the expected 1.1V on the PD lines
-#define GPIO_CC1_IN 0 // ^-^ 连接MUN5233集电极. PD RX Path: Converts the 0-1.2V CC signal to 3.3V using a MUN5233 transistor and the IO in weak mode as a pull up.
+#define GPIO_PD 4     // 可选，正常5.1k下拉CC1即可。PD协议开关，经5.1k电阻连接到CC1。
+// #define GPIO_CC1 3 // 可选：与GPIO_TX同时对CC线弱驱动，形成~1.7V分压器。 原理：两个GPIO都设为输出+DRIVE_CAP_0(最弱驱动)，一个输出高、一个输出低，互相较劲得到中间电压。ESP32内部上拉、下拉电阻为47K
+#define GPIO_TX 3     // ^-^ 连接USB-CC1。TX发送脚，发送时切为输出，完成后切回输入。
+#define GPIO_CC1_IN 0 // ^-^ 连接MUN5233集电极。RX接收：晶体管将CC的0~1.2V信号转换为3.3V逻辑电平。GPIO设为弱输出高(等效上拉)。
 
 #define PD_BUFFER_COUNT 64
 
